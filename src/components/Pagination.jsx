2555a76ef96a9f9f65d5c7d3a08e2cfbd3f1b1f4
asrisitorus04 <asrisitorus04@gmail.com>
2022-10-24 21:48:41 +0700

    make component pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
-export const Pagination = () => {
+const Pagination = (paginateFront, paginateBack) => {
   return (
-    <div>Pagination</div>
+    <>
+    <div>
+        <nav className='inline-flex' aria-label='Pagination'>
+            <a onClick={() => {paginateBack(); 
+            }} href='#' className='inline-flex items-center px-2 py-2 font-medium text-[#2D2D2D] hover:text-gray-500 hover:font-semibold'>
+                <span>Prev</span>
+            </a>
+
+            <a onClick={() => {paginateFront();
+            }} href='#' className='inline-flex items-center px-2 py-2 font-medium text-[#2D2D2D] hover:text-gray-500 hover:font-semibold'>
+                <span>Next</span>
+            </a>
+        </nav>
+    </div>
+    </>
   )
 }
+
+export default Pagination
