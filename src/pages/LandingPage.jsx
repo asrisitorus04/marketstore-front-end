650dcd353952649e0647e5caa16e33d26c7d56b0
dwifakhri <fakhri.chusaini01@gmail.com>
2022-10-24 17:18:35 +0700

    navbaranonym

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { WithRouter } from "../utils/navigation";
 import Layout from "../components/Layout";
 import { useTitle } from "../utils/hooks/useTitle";
+import AnonymNavbar from "../components/AnonymNavbar"
 
 const LandingPage = () => {
   useTitle("Kelontongpedia");
   return (
     <Layout>
+      <AnonymNavbar></AnonymNavbar>
       <div>LandingPage</div>
     </Layout>
   );
