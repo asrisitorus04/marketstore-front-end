438da103de4e75e7905722187b454e56e7172ca4
dwifakhri <fakhri.chusaini01@gmail.com>
2022-10-24 22:02:56 +0700

    notfoundpage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Navbar from "../components/Navbar"
+import { Link } from "react-router-dom";
 import { useTitle } from "../utils/hooks/useTitle";
 
 const NotFoundPage = () => {
   useTitle("404 Not Found");
   return (
     <Layout>
-      <div>NotFoundPage</div>
+      <Navbar></Navbar>
+      <div className="text-center">
+          <h2 className="text-black font-bold text-6xl mt-40">
+            404 - Sorry, Page Not Found
+          </h2>
+          <Link to={"/home"}>
+            <button className="mt-11 text-xs px-14 py-3 rounded-2xl text-white font-semibold bg-primary">
+              RETURN TO HOME
+            </button>
+          </Link>
+        </div>
     </Layout>
   );
 };
