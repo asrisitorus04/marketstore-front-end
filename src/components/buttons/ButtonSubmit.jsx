ba585d96af17162e45d995fd188c44035546eaad
asrisitorus04 <asrisitorus04@gmail.com>
2022-10-24 20:33:51 +0700

    update button

--- src/components/buttons/ButtonSubmit.jsx
+++ src/components/buttons/ButtonSubmit.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Submit = () => {
   return (
     <div>
-        <button className="login px-4 py-2 absolute rounded-md text-white border-4 border-[#358600] shadow-lg transform active:scale-75 transition-transform mx-5 flex">
+        <button className="login px-4 py-2 rounded-md text-white font-semibold border-2 border-[#358600] shadow-lg transform active:scale-75 transition-transform mx-5 flex">
             <span className="text-login text-[#358600] text-center ">SUBMIT</span>
         </button>
       </div>
@@ -13,7 +13,7 @@ export const Submit = () => {
 export const Signup = () => {
     return (
       <div>
-          <button className="login px-4 py-2 absolute rounded-md text-white border-4 border-[#358600] shadow-lg transform active:scale-75 transition-transform mx-5 flex">
+          <button className="login px-4 py-2 rounded-md text-white font-semibold border-2 border-[#358600] shadow-lg transform active:scale-75 transition-transform mx-5 flex">
             <span className="text-login text-[#358600] text-center ">SIGN UP</span>
         </button>
         </div>
@@ -23,7 +23,7 @@ export const Signup = () => {
   export const Login = () => {
     return (
       <div>
-          <button className="login px-4 py-2 absolute rounded-md text-white border-4 border-[#358600] shadow-lg transform active:scale-75 transition-transform mx-5 flex">
+          <button className="login px-4 py-2 rounded-md text-white font-semibold border-2 border-[#358600] shadow-lg transform active:scale-75 transition-transform mx-5 flex">
               <span className="text-login text-[#358600] text-center ">LOGIN</span>
           </button>
         </div>
