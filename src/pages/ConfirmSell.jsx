d6937f02e61ea2721ace5547f740447830dafb34
asrisitorus04 <asrisitorus04@gmail.com>
2022-10-25 17:50:28 +0700

    Slicing page Confirm Sell History

--- src/pages/ConfirmSell.jsx
+++ src/pages/ConfirmSell.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Soldout } from "../components/buttons/ButtonAdd";
+import { SellHisCard } from "../components/Card";
+import Footer from "../components/Footer";
+import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
 
 export const ConfirmSell = () => {
   return (
-    <div>ConfirmSell</div>
-  )
-}
+    <Layout>
+      <Navbar/>
+      <div className="m-10">
+        <div className="font-bold text-center text-[#2D2D2D] text-5xl">
+          Sell History
+        </div>
+        <div className="mt-10">
+          <div className="w-full flex justify-center">
+            <div className="text-center flex flex-col w-3/4 p-4 items-center justify-center gap-10 border-2 border-secondary shadow-lg rounded-lg bg-white">
+              <div>
+                <SellHisCard />
+              </div>
+              <div className="relative bottom-0 ml-auto">
+                <Soldout />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-10">
+          <div className="w-full flex justify-center">
+            <div className="text-center flex flex-col w-3/4 p-4 items-center justify-center gap-10 border-2 border-secondary shadow-lg rounded-lg bg-white">
+              <div>
+                <SellHisCard />
+              </div>
+              <div className="relative bottom-0 ml-auto">
+                <Soldout />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer/>
+    </Layout>
+  );
+};
--- src/pages/PurchasePage.jsx
+++ src/pages/PurchasePage.jsx
@@ -1,35 +1,45 @@
-import React from 'react'
-import { Confirmed } from '../components/buttons/ButtonAdd'
-import { PurchaseHisCard } from '../components/Card'
-import Footer from '../components/Footer'
-import Layout from '../components/Layout'
-import Navbar from '../components/Navbar'
+import React from "react";
+import { Confirmed } from "../components/buttons/ButtonAdd";
+import { PurchaseHisCard } from "../components/Card";
+import Footer from "../components/Footer";
+import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
 
 export default function PurchasePage() {
   return (
     <Layout>
-    <Navbar/>
-    <div className='m-10'>
-        <div className='font-bold text-center text-[#2D2D2D] text-5xl'>Purchase History</div>
-        <div className='mt-10'>
-        <div className='w-full flex justify-center'>
-            <div className='text-center flex flex-col w-3/4 p-4 items-center justify-center gap-10 border-2 border-secondary shadow-lg rounded-lg bg-white'>
-                <div><PurchaseHisCard/></div>
-                <div className='relative bottom-0 ml-auto'><Confirmed/></div>
-            </div>
+      <Navbar />
+      <div className="m-10">
+        <div className="font-bold text-center text-[#2D2D2D] text-5xl">
+          Purchase History
         </div>
+        <div className="mt-10">
+          <div className="w-full flex justify-center">
+            <div className="text-center flex flex-col w-3/4 p-4 items-center justify-center gap-10 border-2 border-secondary shadow-lg rounded-lg bg-white">
+              <div>
+                <PurchaseHisCard />
+              </div>
+              <div className="relative bottom-0 ml-auto">
+                <Confirmed />
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div className='mt-10'>
-        <div className='w-full flex justify-center'>
-            <div className='text-center flex flex-col w-3/4 p-4 items-center justify-center gap-10 border-2 border-secondary shadow-lg rounded-lg bg-white'>
-                <div><PurchaseHisCard/></div>
-                <div className='relative bottom-0 ml-auto'><Confirmed/></div>
+        <div className="mt-10">
+          <div className="w-full flex justify-center">
+            <div className="text-center flex flex-col w-3/4 p-4 items-center justify-center gap-10 border-2 border-secondary shadow-lg rounded-lg bg-white">
+              <div>
+                <PurchaseHisCard />
+              </div>
+              <div className="relative bottom-0 ml-auto">
+                <Confirmed />
+              </div>
             </div>
+          </div>
         </div>
-        </div>
-    </div>
-    <Footer/>
+      </div>
+      <Footer />
     </Layout>
-  )
+  );
 }
