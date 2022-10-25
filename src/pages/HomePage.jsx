aff804af35dd40ae21a2b5fb04dbf79c747dedaa
dwifakhri <fakhri.chusaini01@gmail.com>
2022-10-25 17:31:11 +0700

    slicinghomepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,35 @@ import React from "react";
 import { WithRouter } from "../utils/navigation";
 import Layout from "../components/Layout";
 import { useTitle } from "../utils/hooks/useTitle";
+import Navbar from "../components/Navbar"
+import {ProductCard} from "../components/Card"
+import Footer from "..//components/Footer"
+import Pagination from "../components/Pagination";
+import Carousel from "../components/Carousel"
 
 const HomePage = () => {
   useTitle("Kelontongpedia");
   return (
     <Layout>
-      <div>HomePage</div>
+      <Navbar/>
+      <div className="w-full p-10 flex flex-col space-y-10">
+          <Carousel/>
+          <h1 className="font-semibold text-center text-2xl">All Products</h1>
+          <div className="w-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-10 md:gap-5 px-5 lg:px-20 lg:gap-y-12">
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+          </div>
+          <div className="w-full text-center">
+            <Pagination/>
+          </div>
+        </div>
+        <Footer/>
     </Layout>
   );
 };
