ca7280d1fa562e0fad932abb37a9c800e7034c2e
asrisitorus04 <asrisitorus04@gmail.com>
2022-10-25 17:36:08 +0700

    Slicing page Sell History

--- src/components/buttons/ButtonAdd.jsx
+++ src/components/buttons/ButtonAdd.jsx
@@ -78,8 +78,8 @@ export const Confirmedout = () => {
 export const Sold = () => {
   return (
     <div>
-      <button className="checkout px-4 py-2 bg-primary border-2 border-primary absolute rounded-md text-white shadow-lg transform active:scale-75 transition-transform mx-5 flex hover:bg-white hover:text-primary">
-        <span className="text-checkout ml-2">Sold</span>
+      <button className="checkout items-center justify-center font-extrabold px-4 py-2 bg-white border-2 border-primary rounded-md text-primary shadow-lg transform active:scale-75 transition-transform mx-5 flex hover:bg-white hover:text-primary">
+        <span>Sold</span>
       </button>
     </div>
   );
